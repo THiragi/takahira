6fc5c175376db39b40c14a203c5ea6f70ded5e83
THiragi <w1th1nmybr41n@gmail.com>
2021-03-31 17:55:45 +0900

    comment out shiki packages in lib/transpiler.ts

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -3,18 +3,18 @@ import unified from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
-import rehypeShiki from '@leafac/rehype-shiki';
-import * as shiki from 'shiki';
+// import rehypeShiki from '@leafac/rehype-shiki';
+// import * as shiki from 'shiki';
 
 export const markdownToHtml = async (markdown: string) =>
   unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeShiki, {
-      highlighter: await shiki.getHighlighter({
-        theme: 'nord',
-      }),
-    })
+    // .use(rehypeShiki, {
+    //   highlighter: await shiki.getHighlighter({
+    //     theme: 'nord',
+    //   }),
+    // })
     .use(rehypeStringify)
     .processSync(markdown);
 
