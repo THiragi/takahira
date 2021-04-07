f201590a39d73064c230134cd3e10ba3d71446f2
THiragi <w1th1nmybr41n@gmail.com>
2021-04-07 20:20:40 +0900

    add the shiki-themes directory

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import fs from 'fs';
+import path from 'path';
 import unified from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
@@ -6,13 +8,15 @@ import rehypeStringify from 'rehype-stringify';
 import rehypeShiki from '@leafac/rehype-shiki';
 import * as shiki from 'shiki';
 
+const shikiTheme = path.join(process.cwd(), 'src/lib/shiki-themes/nord.json');
+
 export const markdownToHtml = async (markdown: string) =>
   unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeShiki, {
       highlighter: await shiki.getHighlighter({
-        paths: { themes: './shiki-themes/nord.json' },
+        theme: JSON.parse(fs.readFileSync(shikiTheme, 'utf-8')),
       }),
     })
     .use(rehypeStringify)
