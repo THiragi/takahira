2d51c37f992190244732fa1a9080d69348e65d37
THiragi <w1th1nmybr41n@gmail.com>
2021-04-07 17:33:45 +0900

    specify the shiki-theme with a path

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -4,9 +4,7 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import rehypeShiki from '@leafac/rehype-shiki';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const shiki = require('shiki');
+import * as shiki from 'shiki';
 
 export const markdownToHtml = async (markdown: string) =>
   unified()
@@ -14,7 +12,7 @@ export const markdownToHtml = async (markdown: string) =>
     .use(remarkRehype)
     .use(rehypeShiki, {
       highlighter: await shiki.getHighlighter({
-        theme: 'nord',
+        paths: { themes: './shiki-themes/nord.json' },
       }),
     })
     .use(rehypeStringify)
