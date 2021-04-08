a4e261c64666a41c5ddee4463a4aa5a3b2a7c52e
THiragi <w1th1nmybr41n@gmail.com>
2021-04-08 10:21:55 +0900

    config shiki highlighter options

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// import fs from 'fs';
+import path from 'path';
 import unified from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
@@ -6,13 +8,36 @@ import rehypeStringify from 'rehype-stringify';
 import rehypeShiki from '@leafac/rehype-shiki';
 import * as shiki from 'shiki';
 
+const t = shiki.loadTheme(
+  path.join(process.cwd(), 'src/lib/shiki/themes/nord.json'),
+);
+
 export const markdownToHtml = async (markdown: string) =>
   unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeShiki, {
       highlighter: await shiki.getHighlighter({
-        theme: 'material-theme-palenight',
+        theme: await t,
+        langs: [
+          {
+            id: 'typescript',
+            scopeName: 'source.ts',
+            path: path.join(
+              process.cwd(),
+              'src/lib/shiki/languages/typescript.tmLanguage.json',
+            ),
+            aliases: ['ts'],
+          },
+          {
+            id: 'tsx',
+            scopeName: 'source.tsx',
+            path: path.join(
+              process.cwd(),
+              'src/lib/shiki/languages/tsx.tmLanguage.json',
+            ),
+          },
+        ],
       }),
     })
     .use(rehypeStringify)
