819f301f352493364a25d07ace650504d448f5e9
THiragi <w1th1nmybr41n@gmail.com>
2021-04-08 09:12:54 +0900

    Revert "change tsconfig"
    This reverts commit 2be230c84f584a267832d7edf044c38738323b90.

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import fs from 'fs';
-import path from 'path';
 import unified from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
@@ -8,26 +6,13 @@ import rehypeStringify from 'rehype-stringify';
 import rehypeShiki from '@leafac/rehype-shiki';
 import * as shiki from 'shiki';
 
-const shikiTheme = path.join(process.cwd(), 'src/lib/shiki/themes/nord.json');
-const languages = path.join(
-  process.cwd(),
-  'src/lib/shiki/languages/abap.tmLanguage.json',
-);
-
 export const markdownToHtml = async (markdown: string) =>
   unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeShiki, {
       highlighter: await shiki.getHighlighter({
-        theme: JSON.parse(fs.readFileSync(shikiTheme, 'utf-8')),
-        langs: [
-          {
-            id: 'abap',
-            scopeName: 'source.abap',
-            path: languages,
-          },
-        ],
+        theme: 'material-theme-palenight',
       }),
     })
     .use(rehypeStringify)
