d7234c3ca2f1fb90f2a550880b54eb2d372b9bd6
THiragi <w1th1nmybr41n@gmail.com>
2021-04-11 00:26:09 +0900

    fixed meta twitter: site

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -38,7 +38,7 @@ const Container: React.FC<Props> = ({
         <meta property="og:title" content={title} />
         <meta property="og:image" content={image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@leeerob" />
+        <meta name="twitter:site" content="@levelwood1" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
