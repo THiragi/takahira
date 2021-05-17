82b4f15e74ff739927dd38a69d4c6bbe9b772f4a
THiragi <w1th1nmybr41n@gmail.com>
2021-05-17 11:43:55 +0900

    add: About page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { NextPage } from 'next';
+
+import Container from '../../components/container';
+
+const About: NextPage = () => (
+  <Container section="about" title="about - takahira">
+    <div>
+      <p>こんにちは、takahiraです。</p>
+    </div>
+  </Container>
+);
+
+export default About;
