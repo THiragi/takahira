9e3e7e1d04bb08908b4fb36ec83fd786d82ac5b9
THiragi <w1th1nmybr41n@gmail.com>
2021-05-17 22:34:38 +0900

    add navbar to container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -4,8 +4,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import Header from '../header';
 import Footer from '../footer';
-// import Link from 'next/link';
-
+import Nav from '../nav';
 import styles from './index.module.scss';
 
 type Props = {
@@ -25,36 +24,39 @@ const Container: React.FC<Props> = ({
   const image = 'https://takahira.io/static/images/banner.png';
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>{title}</title>
-        <meta content={description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://takahira.io${router.asPath}`}
-        />
-        <link rel="canonical" href={`https://takahira.io${router.asPath}`} />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="takahira" />
-        <meta property="og:description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:image" content={image} />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@levelwood1" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
-        <meta name="theme-color" content="#404040" />
-      </Head>
-      <Header section={section} />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>{title}</title>
+          <meta content={description} name="description" />
+          <meta
+            property="og:url"
+            content={`https://takahira.io${router.asPath}`}
+          />
+          <link rel="canonical" href={`https://takahira.io${router.asPath}`} />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="takahira" />
+          <meta property="og:description" content={description} />
+          <meta property="og:title" content={title} />
+          <meta property="og:image" content={image} />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@levelwood1" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta name="twitter:image" content={image} />
+          <meta name="theme-color" content="#404040" />
+        </Head>
+        <Header section={section} />
+        <main>{children}</main>
+        <Footer />
+      </div>
+      <Nav />
+    </>
   );
 };
 
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './index.module.scss';
 
 const Nav: React.VFC = () => (
-  <div className={styles.nav}>
-    <nav>nav</nav>
+  <div className={styles.navbar}>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>about</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>blog</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 );
 
