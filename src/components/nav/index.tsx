1a29cdc822d81ff010750d9c8948cf7c36bd25e0
THiragi <w1th1nmybr41n@gmail.com>
2021-05-17 11:49:38 +0900

    add: Nav component

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const Nav: React.VFC = () => (
+  <div className={styles.nav}>
+    <nav>nav</nav>
+  </div>
+);
+
+export default Nav;
