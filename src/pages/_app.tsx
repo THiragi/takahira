792fd614c64f0bf7cb431cca676d173a23e24644
THiragi <w1th1nmybr41n@gmail.com>
2022-03-05 21:33:04 +0900

    add: hightlight.js/styles/nord.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
+import 'highlight.js/styles/nord.css';
 // import '../styles/variables.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
