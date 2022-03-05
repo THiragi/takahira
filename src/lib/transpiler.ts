eb7d251a7eb6df75668503b4ab61cc4615c9a344
THiragi <w1th1nmybr41n@gmail.com>
2022-03-05 21:22:48 +0900

    add rehype-highlight and remove shiki

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -2,25 +2,13 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
-import rehypeShiki from '@leafac/rehype-shiki';
-import { getHighlighter } from 'shiki';
-import fs from 'fs';
-
-// FIXME: Allows shiki to access production theme/language files.
-const path = process.cwd();
-fs.readdirSync(`${path}/node_modules/shiki/languages`);
-fs.readdirSync(`${path}/node_modules/shiki/themes`);
-fs.readdirSync(`${path}/node_modules/vscode-textmate`);
+import rehypeHighlight from 'rehype-highlight';
 
 export const markdownToHtml = async (markdown: string) =>
   unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeShiki, {
-      highlighter: await getHighlighter({
-        theme: 'slack-dark',
-      }),
-    })
+    .use(rehypeHighlight)
     .use(rehypeStringify)
     .processSync(markdown);
 
