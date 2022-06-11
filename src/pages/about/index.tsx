fe352c5a240c93d4e71b1d07d690f338a0ecb86b
THiragi <w1th1nmybr41n@gmail.com>
2022-06-11 15:23:22 +0900

    update about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,6 +7,10 @@ import Profile from '../../components/Profile';
 const About: NextPage = () => (
   <Container section="about" title="about - takahira">
     <Profile />
+    <section>
+      <p>以前はホスピタリティ業界で働いていました。</p>
+      <p>現在はWeb業界の末席でWebエンジニアっぽいことをやっています。</p>
+    </section>
   </Container>
 );
 
